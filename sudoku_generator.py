6a6b13cfaef20912e96166c1f61857821368fba6
Samuel Lapidot <150305386+Sam-Lap@users.noreply.github.com>
2023-12-29 13:56:06 -0800

    Update generator logic with class implementation

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -2,74 +2,62 @@
 Sudoku Generator
 '''
 import random
-from copy import deepcopy
-from sudoku_solver import solve_sudoku, print_board
+import copy 
+from sudoku_solver import SudokuConstraints, solve_sudoku, print_board
 
 class SudokuGenerator:
     def __init__(self):
         self.board = [[0 for _ in range(9)] for _ in range(9)]
-        self.rows_available = [[i for i in range(1, 10)] for _ in range(9)]
-        self.cols_available = [[i for i in range(1, 10)] for _ in range(9)]
-        self.blocks_available = [[i for i in range(1, 10)] for _ in range(9)]
+        self.constraints = SudokuConstraints(self.board)
 
-    def generate_full_board(self):
-        self.fill_board(0, 0)
-
-    def fill_board(self, row, col):
-        if row == 9:
+    def fill_board(self, row=0, col=0):
+        if row == 9: 
             return True
-        if col == 9:
+        if col == 9: 
             return self.fill_board(row + 1, 0)
-
-        block = (row // 3) * 3 + col // 3
-        possible_nums = set(self.rows_available[row]) & set(self.cols_available[col]) & set(self.blocks_available[block])
-        nums = list(possible_nums)
-        random.shuffle(nums)
-
-        for num in nums:
-            if self.place_number(num, row, col):
+        
+        options = random.sample(range(1, 10), 9)
+        
+        for num in options:
+            if self.constraints.is_number_valid(row, col, num):
+                self.board[row][col] = num
+                self.constraints.add_number(row, col, num)
+                
                 if self.fill_board(row, col + 1):
                     return True
-                self._remove_number(num, row, col)
+                
+                self.board[row][col] = 0
+                self.constraints.remove_number(row, col, num)
         return False
 
-    def place_number(self, num, row, col):
-        block = (row // 3) * 3 + col // 3
-        self.board[row][col] = num
-        self.rows_available[row].remove(num)
-        self.cols_available[col].remove(num)
-        self.blocks_available[block].remove(num)
-        return True
-
-    def _remove_number(self, num, row, col):
-        block = (row // 3) * 3 + col // 3
-        self.board[row][col] = 0
-        self.rows_available[row].append(num)
-        self.cols_available[col].append(num)
-        self.blocks_available[block].append(num)
-
-    def remove_numbers(self, difficulty):
-        remaining = 81
-        target = 81 - difficulty
-
-        while remaining > target:
-            row, col = random.randint(0, 8), random.randint(0, 8)
+    def remove_numbers(self, num_to_remove):
+        
+        while num_to_remove > 0: 
+            row, col= random.randint(0, 8), random.randint(0, 8)
+            
+            # while self.board[row][col] == 0:
+            #     row = random.randint(0, 8)
+            #     col = random.randint(0, 8)
             if self.board[row][col] != 0:
                 backup = self.board[row][col]
                 self.board[row][col] = 0
+
+                # Check if the board still has a unique solution
+                copy_board = copy.deepcopy(self.board)
                 
-                board_copy = deepcopy(self.board)
-                if solve_sudoku(board_copy) and self._is_unique_solution(self.board):
-                    remaining -= 1
+                if solve_sudoku(copy_board) and self.is_unique(copy_board):
+                    num_to_remove -= 1
                 else:
                     self.board[row][col] = backup
+            
 
-    def _is_unique_solution(self, board):
+    def is_unique(self, board):
         self.solution_count = 0
-        self._solve_and_count_solutions(deepcopy(board), 0, 0)
+        self.count_solutions(copy.deepcopy(board), 0, 0)
         return self.solution_count == 1
-
-    def _solve_and_count_solutions(self, board, row, col):
+        
+        
+    def count_solutions(self, board, row, col):
         if row == 9:
             self.solution_count += 1
             return
@@ -77,40 +65,40 @@ class SudokuGenerator:
         next_row, next_col = (row, col + 1) if col < 8 else (row + 1, 0)
 
         if board[row][col] == 0:
-            for num in range(1, 10):
-                if self._is_valid(num, row, col, board):
-                    board[row][col] = num
-                    self._solve_and_count_solutions(board, next_row, next_col)
+            for number in range(1, 10):
+                if self.constraints.is_number_valid(row, col, number):
+                    board[row][col] = number
+                    self.count_solutions(board, next_row, next_col)
                     board[row][col] = 0
                     if self.solution_count > 1:
-                        return  # Early exit if more than one solution is found
+                        return
         else:
-            self._solve_and_count_solutions(board, next_row, next_col)
-
-    def _is_valid(self, num, row, col, board):
-        for i in range(9):
-            if board[row][i] == num or board[i][col] == num:
-                return False
-        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
-        for i in range(start_row, start_row + 3):
-            for j in range(start_col, start_col + 3):
-                if board[i][j] == num:
-                    return False
-        return True
-
-    def generate_puzzle(self):
-        self.generate_full_board()
-        self.remove_numbers(37)  # Adjust difficulty here
-        return self.board
-
-if __name__ == "__main__":
-    generator = SudokuGenerator()
-    puzzle = generator.generate_puzzle()
-    print("Generated Puzzle")
-    print_board(puzzle)
-    
-    # if solve_sudoku(puzzle):
-    #     print("Solved Puzzle")
-    #     print_board(puzzle)
-    
+            self.count_solutions(board, next_row, next_col)
         
+
+    def print_board(self):
+        for row in range(9):
+            if row % 3 == 0 and row != 0:
+                print("══════╬═══════╬══════")
+
+            for col in range(9):
+                if col % 3 == 0 and col != 0:
+                    print("║", end=" ")
+                    
+                val = self.board[row][col]
+                print_val = val if val != 0 else "-"
+                print(print_val, end=" ")
+                    
+                if col == 8:
+                    print()
+
+
+# Example Usage
+generator = SudokuGenerator()
+generator.fill_board()
+print("Complete Board:")
+generator.print_board()
+
+generator.remove_numbers(30)  # Remove 50 numbers for example
+print("\nPuzzle with Removed Numbers:")
+generator.print_board()
